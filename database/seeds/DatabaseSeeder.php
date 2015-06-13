85fd01ce71acabe7089d5bf2d566ee63f8c8b822
Matteo Rosati <rosati.matteo@gmail.com>
2015-06-13 11:05:22 +0200

    add some seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+
 class DatabaseSeeder extends Seeder {
 
 	/**
@@ -14,7 +15,57 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UsersTableSeeder');
+		$this->call('ProjectsTableSeeder');
+		$this->call('ActivitiesTableSeeder');
 	}
 
+}
+
+class UsersTableSeeder extends Seeder {
+	public function run()
+	{
+		DB::table('users')->delete();
+		$faker = Faker\Factory::create();
+
+		for ($i = 0; $i < 50; $i++) {
+			App\User::create([
+				'email' => $faker->email,
+				'first_name' => $faker->firstName,
+				'last_name' => $faker->lastName,
+				'password' => Hash::make('123456')
+			]);
+		}
+	}
+}
+
+class ProjectsTableSeeder extends Seeder {
+	public function run()
+	{
+		DB::table('projects')->delete();
+		$faker = Faker\Factory::create();
+
+		for ($i = 0; $i < 20; $i++) {
+			App\Project::create([
+				'name' => $faker->company
+			]);
+		}
+	}
+}
+
+class ActivitiesTableSeeder extends Seeder {
+	public function run()
+	{
+		DB::table('activities')->delete();
+		$faker = Faker\Factory::create();
+
+		for ($i = 0; $i < 150; $i++) {
+			App\Activity::create([
+				'project_id' => $faker->numberBetween($min=1, $max=20),
+				'user_id' => $faker->numberBetween($min=1, $max=50),
+				'start_time' => $faker->dateTimeThisYear($max = 'now'),
+				'end_time' => $faker->dateTimeThisYear($max = 'now')
+			]);
+		}
+	}
 }
