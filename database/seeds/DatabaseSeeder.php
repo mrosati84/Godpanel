94e0dfb88f32f9e0c0cbdb140d156b3a7537a72c
Matteo Rosati <rosati.matteo@gmail.com>
2015-06-14 18:00:23 +0200

    update seeder with "real" data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -26,18 +26,11 @@ class UsersTableSeeder extends Seeder {
 	public function run()
 	{
 		DB::table('users')->delete();
-		$faker = Faker\Factory::create();
-		$areas = ['DEV', 'PM', 'UX', 'ART', 'CLIENT', 'STRATEGY'];
 
-		for ($i = 0; $i < 50; $i++) {
-			App\User::create([
-				'email' => $faker->email,
-				'area' => array_rand($areas, $num=1),
-				'first_name' => $faker->firstName,
-				'last_name' => $faker->lastName,
-				'password' => Hash::make('123456')
-			]);
-		}
+		App\User::create(['email' => 'mrosati@h-art.com', 'area' => 'DEV', 'first_name' => 'Matteo', 'last_name' => 'Rosati', 'password' => Hash::make('123456')]);
+		App\User::create(['email' => 'mfiaschini@h-art.com', 'area' => 'DEV', 'first_name' => 'Maurizio', 'last_name' => 'Fiaschini', 'password' => Hash::make('123456')]);
+		App\User::create(['email' => 'lcadamuro.ce@h-art.com', 'area' => 'DEV', 'first_name' => 'Lorenzo', 'last_name' => 'Cadamuro', 'password' => Hash::make('123456')]);
+		App\User::create(['email' => 'nruggeri@h-art.com', 'area' => 'DEV', 'first_name' => 'Nicholas', 'last_name' => 'Ruggeri', 'password' => Hash::make('123456')]);
 	}
 }
 
@@ -45,13 +38,11 @@ class ProjectsTableSeeder extends Seeder {
 	public function run()
 	{
 		DB::table('projects')->delete();
-		$faker = Faker\Factory::create();
 
-		for ($i = 0; $i < 20; $i++) {
-			App\Project::create([
-				'name' => $faker->company
-			]);
-		}
+		App\Project::create(['name' => 'Telecom']);
+		App\Project::create(['name' => 'Mutti']);
+		App\Project::create(['name' => 'Silversea']);
+		App\Project::create(['name' => 'Intesa San Paolo']);
 	}
 }
 
@@ -59,15 +50,36 @@ class ActivitiesTableSeeder extends Seeder {
 	public function run()
 	{
 		DB::table('activities')->delete();
-		$faker = Faker\Factory::create();
 
 		for ($i = 0; $i < 150; $i++) {
-			App\Activity::create([
-				'project_id' => $faker->numberBetween($min=1, $max=20),
-				'user_id' => $faker->numberBetween($min=1, $max=50),
-				'start_time' => $faker->dateTimeThisYear($max = 'now'),
-				'end_time' => $faker->dateTimeThisYear($max = 'now')
-			]);
+			App\Activity::create(['project_id' => 1, 'user_id' => 1, 'note' => null, 'start_time' => '2015-06-01 09:00:00', 'end_time' => '2015-06-01 10:00:00']);
+			App\Activity::create(['project_id' => 1, 'user_id' => 1, 'note' => null, 'start_time' => '2015-06-01 10:00:00', 'end_time' => '2015-06-01 11:00:00']);
+			App\Activity::create(['project_id' => 1, 'user_id' => 1, 'note' => null, 'start_time' => '2015-06-01 11:00:00', 'end_time' => '2015-06-01 12:00:00']);
+			App\Activity::create(['project_id' => 1, 'user_id' => 1, 'note' => null, 'start_time' => '2015-06-01 12:00:00', 'end_time' => '2015-06-01 13:00:00']);
+			App\Activity::create(['project_id' => 1, 'user_id' => 1, 'note' => null, 'start_time' => '2015-06-01 13:00:00', 'end_time' => '2015-06-01 14:00:00']);
+			App\Activity::create(['project_id' => 1, 'user_id' => 1, 'note' => null, 'start_time' => '2015-06-01 14:00:00', 'end_time' => '2015-06-01 15:00:00']);
+			App\Activity::create(['project_id' => 1, 'user_id' => 1, 'note' => null, 'start_time' => '2015-06-01 15:00:00', 'end_time' => '2015-06-01 16:00:00']);
+			App\Activity::create(['project_id' => 1, 'user_id' => 1, 'note' => null, 'start_time' => '2015-06-01 16:00:00', 'end_time' => '2015-06-01 17:00:00']);
+
+			App\Activity::create(['project_id' => 2, 'user_id' => 2, 'note' => null, 'start_time' => '2015-06-01 09:00:00', 'end_time' => '2015-06-01 10:00:00']);
+			App\Activity::create(['project_id' => 2, 'user_id' => 2, 'note' => null, 'start_time' => '2015-06-01 10:00:00', 'end_time' => '2015-06-01 11:00:00']);
+			App\Activity::create(['project_id' => 2, 'user_id' => 2, 'note' => null, 'start_time' => '2015-06-01 11:00:00', 'end_time' => '2015-06-01 12:00:00']);
+			App\Activity::create(['project_id' => 2, 'user_id' => 2, 'note' => null, 'start_time' => '2015-06-01 12:00:00', 'end_time' => '2015-06-01 13:00:00']);
+
+			App\Activity::create(['project_id' => 3, 'user_id' => 2, 'note' => null, 'start_time' => '2015-06-01 13:00:00', 'end_time' => '2015-06-01 14:00:00']);
+			App\Activity::create(['project_id' => 3, 'user_id' => 2, 'note' => null, 'start_time' => '2015-06-01 14:00:00', 'end_time' => '2015-06-01 15:00:00']);
+			App\Activity::create(['project_id' => 3, 'user_id' => 2, 'note' => null, 'start_time' => '2015-06-01 15:00:00', 'end_time' => '2015-06-01 16:00:00']);
+			App\Activity::create(['project_id' => 3, 'user_id' => 2, 'note' => null, 'start_time' => '2015-06-01 16:00:00', 'end_time' => '2015-06-01 17:00:00']);
+
+			App\Activity::create(['project_id' => 3, 'user_id' => 3, 'note' => null, 'start_time' => '2015-06-02 13:00:00', 'end_time' => '2015-06-02 14:00:00']);
+			App\Activity::create(['project_id' => 3, 'user_id' => 3, 'note' => null, 'start_time' => '2015-06-02 14:00:00', 'end_time' => '2015-06-02 15:00:00']);
+			App\Activity::create(['project_id' => 3, 'user_id' => 3, 'note' => null, 'start_time' => '2015-06-02 15:00:00', 'end_time' => '2015-06-02 16:00:00']);
+			App\Activity::create(['project_id' => 3, 'user_id' => 3, 'note' => null, 'start_time' => '2015-06-02 16:00:00', 'end_time' => '2015-06-02 17:00:00']);
+
+			App\Activity::create(['project_id' => 4, 'user_id' => 4, 'note' => null, 'start_time' => '2015-06-03 09:00:00', 'end_time' => '2015-06-03 10:00:00']);
+			App\Activity::create(['project_id' => 4, 'user_id' => 4, 'note' => null, 'start_time' => '2015-06-03 10:00:00', 'end_time' => '2015-06-03 11:00:00']);
+			App\Activity::create(['project_id' => 4, 'user_id' => 4, 'note' => null, 'start_time' => '2015-06-03 11:00:00', 'end_time' => '2015-06-03 12:00:00']);
+			App\Activity::create(['project_id' => 4, 'user_id' => 4, 'note' => null, 'start_time' => '2015-06-03 12:00:00', 'end_time' => '2015-06-03 13:00:00']);
 		}
 	}
 }
