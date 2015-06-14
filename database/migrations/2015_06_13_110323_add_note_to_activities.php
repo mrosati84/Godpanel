cef756048c3f6d99a0d77458df0beda4c6cdde22
Matteo Rosati <rosati.matteo@gmail.com>
2015-06-14 17:59:18 +0200

    adding note to activities

---
+++ database/migrations/2015_06_13_110323_add_note_to_activities.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNoteToActivities extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('activities', function(Blueprint $table)
+		{
+			$table->text('note')->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('activities', function(Blueprint $table)
+		{
+			$table->dropColumn('note');
+		});
+	}
+
+}
