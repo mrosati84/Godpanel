2c06891d5b97d6f20f9290aaee02636d7de371bd
Matteo Rosati <rosati.matteo@gmail.com>
2015-06-14 18:00:55 +0200

    add static scheduler route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', 'GodpanelController@index');
 
 Route::group(['prefix' => 'api/v1'], function ()
@@ -22,4 +11,9 @@ Route::group(['prefix' => 'api/v1'], function ()
     Route::delete('projects/{id}', 'ProjectsController@destroy');
 
     Route::get('users', 'UsersController@index');
+});
+
+Route::get('scheduler', function ()
+{
+    return view('scheduler');
 });
