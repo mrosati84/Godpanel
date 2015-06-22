71a4069d7db5ad5b40b14e45daf7248412a263c8
Matteo Rosati <mrosati@h-art.it>
2015-06-22 12:33:24 +0200

    add user permissions

--- app/User.php
+++ app/User.php
@@ -48,4 +48,58 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 		return $this->belongsTo('App\Role');
 	}
 
+	public function getPermissions()
+	{
+		$permissions = explode('|', $this->permissions);
+
+		// handle empty permissions array
+		if (count($permissions) === 1) {
+			if ($permissions[0] === "") {
+				return [];
+			}
+		}
+
+		return $permissions;
+	}
+
+	public function addPermission($permission_name)
+	{
+		$permissions = $this->getPermissions();
+
+		if (in_array($permission_name, $permissions)) {
+			return true; // user already have this permission
+		}
+
+		array_push($permissions, strtoupper($permission_name));
+
+		$this->permissions = implode('|', $permissions);
+
+		return ($this->save());
+	}
+
+	public function removePermission($permission_name)
+	{
+		$permissions = $this->getPermissions();
+		$permission_key = array_search(strtoupper($permission_name), $permissions);
+
+		if ($permission_key !== false) {
+			// permission found
+			unset($permissions[$permission_key]);
+
+			$this->permissions = implode('|', $permissions);
+
+			return ($this->save());
+		}
+
+		return false;
+	}
+
+	public function hasPermission($permission_name)
+	{
+		$permissions = $this->getPermissions();
+		$permission_key = array_search(strtoupper($permission_name), $permissions);
+
+		return $permission_key !== false;
+	}
+
 }
