9b197bfe3548c9cc63adfe0ef952756faf892ffd
Matteo Rosati <mrosati@h-art.it>
2015-06-25 11:19:02 +0200

    update seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -34,11 +34,6 @@ class UsersTableSeeder extends Seeder {
 		App\User::create(['email' => 'user3@domain.com', 'role_id' => 3, 'first_name' => 'User', 'last_name' => '3', 'password' => Hash::make('123456')]);
 		App\User::create(['email' => 'user4@domain.com', 'role_id' => 4, 'first_name' => 'User', 'last_name' => '4', 'password' => Hash::make('123456')]);
 		App\User::create(['email' => 'user5@domain.com', 'role_id' => 5, 'first_name' => 'User', 'last_name' => '5', 'password' => Hash::make('123456')]);
-		App\User::create(['email' => 'user6@domain.com', 'role_id' => 6, 'first_name' => 'User', 'last_name' => '6', 'password' => Hash::make('123456')]);
-		App\User::create(['email' => 'user7@domain.com', 'role_id' => 1, 'first_name' => 'User', 'last_name' => '7', 'password' => Hash::make('123456')]);
-		App\User::create(['email' => 'user8@domain.com', 'role_id' => 2, 'first_name' => 'User', 'last_name' => '8', 'password' => Hash::make('123456')]);
-		App\User::create(['email' => 'user9@domain.com', 'role_id' => 3, 'first_name' => 'User', 'last_name' => '9', 'password' => Hash::make('123456')]);
-		App\User::create(['email' => 'user10@domain.com', 'role_id' => 4, 'first_name' => 'User', 'last_name' => '10', 'password' => Hash::make('123456')]);
 	}
 }
 
@@ -51,9 +46,6 @@ class AreasTableSeeder extends Seeder {
 		App\Area::create(['name' => 'PM']);
 		App\Area::create(['name' => 'UX']);
 		App\Area::create(['name' => 'ART']);
-		App\Area::create(['name' => 'CLIENT']);
-		App\Area::create(['name' => 'SOCIAL']);
-		App\Area::create(['name' => 'STRATEGIC']);
 	}
 }
 
@@ -80,10 +72,6 @@ class ProjectsTableSeeder extends Seeder {
 		App\Project::create(['name' => 'Project 2']);
 		App\Project::create(['name' => 'Project 3']);
 		App\Project::create(['name' => 'Project 4']);
-		App\Project::create(['name' => 'Project 5']);
-		App\Project::create(['name' => 'Project 6']);
-		App\Project::create(['name' => 'Project 7']);
-		App\Project::create(['name' => 'Project 8']);
 	}
 }
 
@@ -92,13 +80,11 @@ class ActivitiesTableSeeder extends Seeder {
 	{
 		DB::table('activities')->delete();
 
-		// App\Activity::create(['project_id' => 1, 'user_id' => 1, 'description' => 'Lorem ipsum', 'start_date' => '2015-06-14 09:00:00', 'end_date' => '2015-06-14 13:00:00']);
-		// App\Activity::create(['project_id' => 2, 'user_id' => 1, 'description' => 'Lorem ipsum', 'start_date' => '2015-06-14 13:00:00', 'end_date' => '2015-06-14 17:00:00']);
-
-		// App\Activity::create(['project_id' => 2, 'user_id' => 2, 'description' => 'Lorem ipsum', 'start_date' => '2015-06-15 13:00:00', 'end_date' => '2015-06-15 17:00:00']);
-
-		// App\Activity::create(['project_id' => 3, 'user_id' => 3, 'description' => 'Lorem ipsum', 'start_date' => '2015-06-15 09:00:00', 'end_date' => '2015-06-15 17:00:00']);
-
-		// App\Activity::create(['project_id' => 4, 'user_id' => 4, 'description' => 'Lorem ipsum', 'start_date' => '2015-06-16 09:00:00', 'end_date' => '2015-06-16 13:00:00']);
+		App\Activity::create(['project_id' => 1, 'user_id' => 1, 'description' => 'Lorem ipsum', 'start_date' => '2015-06-14 09:00:00', 'end_date' => '2015-06-14 13:00:00']);
+		App\Activity::create(['project_id' => 2, 'user_id' => 1, 'description' => 'Lorem ipsum', 'start_date' => '2015-06-14 13:00:00', 'end_date' => '2015-06-14 17:00:00']);
+		App\Activity::create(['project_id' => 2, 'user_id' => 2, 'description' => 'Lorem ipsum', 'start_date' => '2015-06-15 13:00:00', 'end_date' => '2015-06-15 17:00:00']);
+		App\Activity::create(['project_id' => 3, 'user_id' => 3, 'description' => 'Lorem ipsum', 'start_date' => '2015-06-15 09:00:00', 'end_date' => '2015-06-15 17:00:00']);
+		App\Activity::create(['project_id' => 4, 'user_id' => 4, 'description' => 'Lorem ipsum', 'start_date' => '2015-06-16 09:00:00', 'end_date' => '2015-06-16 13:00:00']);
+		App\Activity::create(['project_id' => 4, 'user_id' => 5, 'description' => 'Lorem ipsum', 'start_date' => '2015-06-16 09:00:00', 'end_date' => '2015-06-16 13:00:00']);
 	}
 }
